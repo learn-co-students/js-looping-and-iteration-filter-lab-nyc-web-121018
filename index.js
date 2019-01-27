21d1672337d987020dadd16878ea6250365c28f4
tfuquirk <timothyfquirk@gmail.com>
2019-01-27 15:09:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(function (str) {return str.toLowerCase() === string.toLowerCase();});
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function (str) {return str[0].toLowerCase() === string[0].toLowerCase();});
+}
+
+function matchName(array, string) {
+  return array.filter(function (driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
+}
