b512fc2c708d2281fbaa7aec9ea79db56bc7b66a
Daniel Fries <daniel.w.fries@gmail.com>
2019-01-27 17:35:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, name){
+  return drivers.filter(function(driverName){
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+};
+
+function fuzzyMatch(drivers, letters){
+  return drivers.filter(function(name){
+    return name.slice(0, letters.length) === letters;
+  });
+};
+
+function matchName(driverList, givenName){
+  return driverList.filter(function(driver){
+    return driver.name.toLowerCase() === givenName.toLowerCase();
+  });
+};
