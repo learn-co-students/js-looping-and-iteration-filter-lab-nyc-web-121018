deff2a915a8196449efda00f39fc4ff81146a54b
Nison Malayev <nmalayev@gmail.com>
2019-01-27 23:16:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  const match = drivers.filter(function (driver) {return driver.toUpperCase() === string.toUpperCase() })
+  return match
+}
+
+function fuzzyMatch(drivers, string) {
+  const match = drivers.filter(function (driver) {return driver.slice(0,2) === string})
+  return match;
+}
+
+function matchName(drivers, string) {
+  const match = drivers.filter(function (driver) {return driver.name === string;})
+  return match
+}
